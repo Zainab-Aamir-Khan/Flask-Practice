197ca967f1bba3a04daa50b1e026636f580e0677
Zainab Aamir <zaamir427@gmail.com>
2025-02-07 23:01:17 +0500

    ..

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 db = SQLALchemy(app)
 
 class Item(db.Model):
-    id = db.Column(db.Integer() Primary_key = True) #must do 
+    id = db.Column(db.Integer() primary_key = True) #must do 
     name =db.Column(db.String(length = 30), nullable = False, unique = True )
     price =db.Column(db.Integer(), nullable = False)
     barcode = db.Column(db.String(length = 12), nullable = False, unique = True)
