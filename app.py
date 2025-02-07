88d9ad3a1cb5ff78e8f3d81e02c1cf569578a773
Zainab Aamir <zaamir427@gmail.com>
2025-02-07 22:50:52 +0500

    ..

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLALchemy
 app = Flask(__name__)
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
 db = SQLALchemy(app)
 
 class Item(db.Model):
